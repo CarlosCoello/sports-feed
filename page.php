b748d5fc7cb87d60a6ba2031e685df6b9b1be80e
Carlos Coello <carlos.coello05@outlook.com>
2017-03-06 00:24:31 -0600

    page file

---
+++ page.php
@@ -0,0 +1,28 @@
+<?php get_header(); ?>
+
+	<div id="primary" class="content-area">
+	
+		<main id="main" class="site-main" role="main">
+          <div class="container"><?php  the_crumbs(); ?></div>
+          
+		<?php
+		if( have_posts() ):
+						
+						while( have_posts() ): the_post(); ?>
+          
+							
+                              
+                               <?php get_template_part( 'template-parts/content', 'page' );?>
+                               
+                            
+						
+				<?php		endwhile;
+						
+					endif;
+		?>
+
+		</main><!-- .site-main -->
+		
+	</div><!-- .content-area -->
+
+<?php get_footer(); ?>
